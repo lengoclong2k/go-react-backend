349441c5e6072a891be77d46d4afab7168a31086
lengoclong <long.lengoc@powergateaustralia.com>
2024-09-25 21:32:58 +0700

    Update

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"backend/internal/models"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world from %s", app.Domain)
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2026-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		Description: "Very nice Movie",
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+
+	movies = append(movies, highlander)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
